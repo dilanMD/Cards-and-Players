1359871c0c69053fbcdbbebd709e6ddd27aaa13f
Deluxan <deluxan.mariathasan@axiatadigitallabs.com>
2020-11-14 20:56:13 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = ()  => {
+  return (
+    <>
+      
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
